b9d7d3defffe878365e67182e7a58784befa63a1
denis-597 <de.kalfov@gmail.com>
2021-09-16 13:09:28 +0300

    added sorting, datejs and some styles

--- sirma-interview-task-employees/src/App.js
+++ sirma-interview-task-employees/src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/styles';
 import { readString } from 'react-papaparse';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+require('datejs');
+
+const styles = theme => ({
+  eachRowStyle: {
+    '& .MuiPaper-root': {
+      padding: 10
+    }
+  },
+});
 
 class App extends Component {
   constructor(props) {
@@ -15,10 +26,10 @@ class App extends Component {
     datePast = datePast.replace(/\s/g, '')
     dateNow = dateNow.replace(/\s/g, '')
     if (dateNow == 'NULL') {
-      dateNow = new Date();
+      dateNow = Date.today();
     }
 
-    const diffTime = Math.abs(new Date(dateNow) - new Date(datePast));
+    const diffTime = Math.abs(Date.parse(dateNow) - Date.parse(datePast));
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
     return diffDays;
   }
@@ -38,78 +49,83 @@ class App extends Component {
             res[parseInt(p[1])] = {}
           }
 
-          if (!res[parseInt(p[1])][p[0]]) { // If object with key [the employee id/name] is not found - create it
-            res[parseInt(p[1])][p[0]] = this.calculcateDiffBetweenDates(p[2], p[3])
+          if (!res[parseInt(p[1])]['empl_' + p[0]]) { // If object with key [the employee id/name] is not found - create it
+            res[parseInt(p[1])]['empl_' + p[0]] = this.calculcateDiffBetweenDates(p[2], p[3])
           } else {
-            res[parseInt(p[1])][p[0]] += this.calculcateDiffBetweenDates(p[2], p[3]) // Sum the value of existing key with the new one
+            res[parseInt(p[1])]['empl_' + p[0]] += this.calculcateDiffBetweenDates(p[2], p[3]) // Sum the value of existing key with the new one
           }
         }
       })
 
+      let sorted = {}
+      Object.values(res).map((r, k) => {
+        sorted[Object.keys(res)[k]] = Object.entries(r) // Sort the object
+          .sort(([, a], [, b]) => b - a)
+          .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+      })
 
-      // console.log(`sortable`, sortable)
-      this.setState({ data: res })
-      console.log(`res Here`, res)
+      this.setState({ data: sorted })
     };
+
     reader.readAsText(e.target.files[0])
-
-
   }
 
   render = () => {
-    console.log(`this.state.data`, this.state.data)
+    const { classes } = this.props;
+
     return (
-      <div>
-        <input type="file" onChange={(e) => this.showFile(e)} />
+      <div style={{ width: '100%', maxWidth: 800, margin: '0 auto', marginTop: 50 }}>
+
+        <input style={{ display: 'none' }} type="file" onChange={(e) => this.showFile(e)} id="button-file" />
+
+        <label htmlFor="button-file">
+          <Button variant="contained" component="span">
+            Upload file
+          </Button>
+        </label>
 
         <br />
         <br />
 
         <Grid container spacing={3}>
 
-          <Grid item xs={3}>
+          <Grid item xs={3} className={classes.eachRowStyle}>
             <Paper>Employee ID #1</Paper>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item xs={3} className={classes.eachRowStyle}>
             <Paper>Employee ID #2</Paper>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item xs={3} className={classes.eachRowStyle}>
             <Paper>Project ID</Paper>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item xs={3} className={classes.eachRowStyle}>
             <Paper>Days worked</Paper>
           </Grid>
 
-          {Object.values(this.state.data).map((r, k) => {
-
-            return Object.values(r).slice(0, 1).map((rr, kk) => {
-              console.log(`rr`, rr)
-              return null
-              return (
-                <>
-                  <Grid item xs={3}>
-                    <Paper>{Object.keys(r)[0]}</Paper>
-                  </Grid>
-                  <Grid item xs={3}>
-                    <Paper>{Object.keys(r)[1]}</Paper>
-                  </Grid>
-                  <Grid item xs={3}>
-                    <Paper>{Object.keys(this.state.data)[k]}</Paper>
-                  </Grid>
-                  <Grid item xs={3}>
-                    <Paper>{rr}+{Object.values(r)[kk+1]}</Paper>
-                  </Grid>
-                </>
-              )
+          {Object.values(this.state.data).map((r, k) => (
+            Object.values(r).slice(0, 1).map((rr, kk) => {
+              return <>
+                <Grid item xs={3} className={classes.eachRowStyle}>
+                  <Paper>{Object.keys(r)[0].split('_')[1]}</Paper>
+                </Grid>
+                <Grid item xs={3} className={classes.eachRowStyle}>
+                  <Paper>{Object.keys(r)[1].split('_')[1]}</Paper>
+                </Grid>
+                <Grid item xs={3} className={classes.eachRowStyle}>
+                  <Paper>{Object.keys(this.state.data)[k]}</Paper>
+                </Grid>
+                <Grid item xs={3} className={classes.eachRowStyle}>
+                  <Paper>{rr + Object.values(r)[kk + 1]}</Paper>
+                </Grid>
+              </>
             })
-
-          })}
+          ))}
 
         </Grid>
 
-      </div>
+      </div >
     )
   }
 }
 
-export default App;
+export default (withStyles(styles)(App));
